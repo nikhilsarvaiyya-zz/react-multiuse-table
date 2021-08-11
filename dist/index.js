7f5e9238625298ac47463ac130033ac91fccf9aa
nikhils <nikhils@aspenify.com>
2021-08-12 02:38:45 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -3,21 +3,45 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Badge", {
-  enumerable: true,
-  get: function get() {
-    return _Badge.default;
-  }
-});
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.default;
-  }
-});
+exports.default = void 0;
 
-var _Badge = _interopRequireDefault(require("./components/Badge"));
+require("core-js/modules/web.dom-collections.iterator.js");
 
-var _Button = _interopRequireDefault(require("./components/Button"));
+var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const MyTable = props => {
+  if (!props.rmtHeaders || !props.rmtData) {
+    return "Loading...";
+  }
+
+  let mapData = [];
+  props.rmtData.forEach(d => {
+    let selectedkey = [];
+    props.rmtHeaders.forEach(h => {
+      for (const [key, value] of Object.entries(d)) {
+        if (h === key) {
+          selectedkey.push(value);
+        }
+      }
+    });
+    mapData.push(selectedkey);
+  });
+  return /*#__PURE__*/_react.default.createElement("table", null, /*#__PURE__*/_react.default.createElement("thead", null, /*#__PURE__*/_react.default.createElement("tr", null, props.rmtHeaders.map((h, i) => {
+    return /*#__PURE__*/_react.default.createElement("th", {
+      key: i
+    }, h);
+  }))), /*#__PURE__*/_react.default.createElement("tbody", null, mapData.map((d, i1) => {
+    return /*#__PURE__*/_react.default.createElement("tr", {
+      key: i1
+    }, d.map((m, i2) => {
+      return /*#__PURE__*/_react.default.createElement("td", {
+        key: i2
+      }, m);
+    }));
+  })));
+};
+
+var _default = MyTable;
+exports.default = _default;
