5b51b2b107805dd789656c5c3dc0cdcbb6bac5d5
nikhils <nikhils@aspenify.com>
2021-09-05 16:25:05 +0530

    defects

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
-
-
-
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <h1>Work In Progress</h1>
     </React.StrictMode>,
     document.getElementById('root')
 );
